31d6f2e99a0492e51c10fbbe055938133e2ddcb0
jggomez <juan.gomez01@gmail.com>
2015-04-07 21:36:06 -0500

    Se sube la primera version

---
+++ PatronSingleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PatronSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton instancia1 = Singleton.ObtenerInstancia();
+
+            Console.WriteLine(String.Format("Instancia Singleton 1 = {0}", instancia1.GetHashCode()));
+
+            Singleton instancia2 = Singleton.ObtenerInstancia();
+
+            Console.WriteLine(String.Format("Instancia Singleton 2 = {0}", instancia2.GetHashCode()));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PatronSingleton/Singleton.cs
@@ -0,0 +1,23 @@
+﻿
+namespace PatronSingleton
+{
+    public class Singleton
+    {
+
+        private static Singleton instancia = null;
+
+        private Singleton(){}
+
+        public static Singleton ObtenerInstancia(){
+
+            if (instancia == null)
+            {
+                instancia = new Singleton();
+            }
+
+            return instancia;
+
+        }
+
+    }
+}
